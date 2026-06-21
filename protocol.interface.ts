a6f86a3d295f1a84f8f99251176750c6a5907c19
Divyansh Parashar <divyanshdp1212@gmail.com>
2026-06-22 03:43:25 +0530

    feat: define RGA operation interfaces

---
+++ protocol.interface.ts
@@ -0,0 +1,20 @@
+import { Timestamp, RGANode, RGA } from "./RGA";
+
+
+export type OperationType = 'insert' | 'delete';
+
+interface InsertOperation {
+    type: 'insert'; //discriminator
+    insertOriginId: Timestamp | null; // lamport id after which we need to insert the new node
+    value: string; // the actual character
+    id: Timestamp; // id of the current node, whic his needed to be inserted
+
+}
+
+interface DeleteOperation {
+    type: 'delete';
+    id: Timestamp; // lamport id of the node to be tombstoned
+}
+
+
+export type OperationMessage = InsertOperation | DeleteOperation;
